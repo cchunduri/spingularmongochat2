7564e6c4ad1c2b7e2f9f50e7508e7df7ea0c734c
Tonterias <ecorreos@htomail.com>
2019-05-14 00:31:02 +0200

    set up chat frontend logic

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,14 +1,19 @@
+import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateMomentAdapter } from './util/datepicker-adapter';
+
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {
   Spingularmongochat2SharedLibsModule,
   Spingularmongochat2SharedCommonModule,
   JhiLoginModalComponent,
-  HasAnyAuthorityDirective
+  HasAnyAuthorityDirective,
+  ChatService
 } from './';
 
 @NgModule({
   imports: [Spingularmongochat2SharedLibsModule, Spingularmongochat2SharedCommonModule],
   declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
+  providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }, ChatService],
   entryComponents: [JhiLoginModalComponent],
   exports: [Spingularmongochat2SharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
